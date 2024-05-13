1fea99c0abd80e7c61838d526fe75dc7f73c686e
aniketverma11 <aniketverma1103@gmail.com>
2024-05-13 18:34:37 +0530

    add list dataset api

--- datahub_backend/dataset/views.py
+++ datahub_backend/dataset/views.py
@@ -6,20 +6,24 @@ from datahub_backend.dataset.serializers import DatasetSerializer, TagSerializer
 
 
 class DatasetViewSet(viewsets.ModelViewSet):
+    permission_classes = ()
     queryset = Dataset.objects.all()
     serializer_class = DatasetSerializer
 
 
 class TagViewSet(viewsets.ModelViewSet):
+    permission_classes = ()
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = ()
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
 class ProviderViewSet(viewsets.ModelViewSet):
+    permission_classes = ()
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
